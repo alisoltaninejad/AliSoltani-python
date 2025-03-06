07b924f5817df797b5e371dc6e6e6c0b607c1b12
Ali Soltani <alisolinejad@gmail.com>
2025-03-06 11:33:24 +0330

    Ali Soltani exercise python check types

---
+++ AliSoltani-1.py
@@ -0,0 +1,22 @@
+print('Ali Soltani | Homework 1 | check types')
+print(type(True))   #'bool'
+print(type(None))   #'NoneType'
+print(type(1234))   #'int'
+print(type(12.5))   #'float'
+print(type('Amin')) #'str'
+print(type(False))  #'bool'
+print(type(float(4)))   #'float'
+print(type(int(3.1)))   #'int'
+print('-----------------------------')#-----------------------------
+print(type(str(20)))   #'str'
+print(type(bool(0)))    #'bool'
+print(type(bool(1)))    #'bool'
+print(type(bool(5)))    #'bool'
+print(type(bool(-5)))    #'bool'
+print(type('a')) #'str'
+#print(type(a)) #Error: name 'a' is not defined
+print(type('a' + 'b')) #'str'
+#print(type('a' +  2)) #TypeError: can only concatenate str (not "int") to str
+print(type('a' + str(2))) #'str'
+print(type(3.0)) #'float'
+print(type('00.2')) #'str'
