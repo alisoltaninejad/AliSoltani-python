375f61a6f641bd2f729501b799cf27c9a44359ef
Ali Soltani <alisolinejad@gmail.com>
2025-05-30 16:49:55 +0330

    Update and rename AliSoltani-project-1.py to AliSoltani-project.py

--- AliSoltani-project.py
+++ AliSoltani-project.py
@@ -1,9 +1,14 @@
-# محاسبه  نمرات دانشجویان و مشخص کردن وضعیت قبولی هر کدام
 students=[
     ('ali',[12,20,18,19]),
     ('reza',[8,14,18]),
     ('samad',[13,3,6]),
 ]
+
+highest_grade = 0
+lowest_grade = 20
+highest_student = ""
+lowest_student = ""
+
 for student in students:
     name= student[0]
     grades=student[1]
@@ -20,3 +25,13 @@ for student in students:
         status = "❌ مردود شد"
 
     print(f'{student[0]}:{average:.1f}=>{status}')
+    
+    if average > highest_grade:
+        highest_grade = average
+        highest_student = name
+    if average < lowest_grade:
+        lowest_grade = average
+        lowest_student = name
+
+print(f"بالاترین نمره: {highest_grade:.1f} {highest_student}")
+print(f"پایین‌ترین نمره: {lowest_grade:.1f} {lowest_student}")
